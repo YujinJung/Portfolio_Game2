f753605ae542b90081af1dffb32a5c88df18f833
YuJin Jung <yujin.jung0@gmail.com>
2019-07-01 14:26:51 +0900

    Crosshair 추가

--- Source/Portfolio_Game2/Private/Portfolio_Game2GameModeBase.cpp
+++ Source/Portfolio_Game2/Private/Portfolio_Game2GameModeBase.cpp
@@ -3,14 +3,18 @@
 
 #include "Portfolio_Game2GameModeBase.h"
 #include "ConstructorHelpers.h"
+#include "CrosshairHUD.h"
 
 APortfolio_Game2GameModeBase::APortfolio_Game2GameModeBase()
 {
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawn(TEXT("/Game/Blueprints/MyCharacter"));
-
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawn(TEXT("/Game/Blueprints/BP_Character"));
 	if (PlayerPawn.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawn.Class;
 	}
-
+	static ConstructorHelpers::FClassFinder<ACrosshairHUD> CrosshairHUD(TEXT("/Game/Blueprints/BP_CrosshairHUD"));
+	if (CrosshairHUD.Class != NULL)
+	{
+		HUDClass = CrosshairHUD.Class;
+	}
 }
